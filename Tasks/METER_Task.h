f33a1eb4d6eb8e6c8bdf627533d496f6260edb5f
Evgenii Kotyshkov <Harakivi@gmail.com>
2023-11-06 14:15:26 +0700

    stack for sprintf

--- Tasks/METER_Task.h
+++ Tasks/METER_Task.h
@@ -6,7 +6,7 @@
 #define FILTER_DATA_INCR_SIZE 50
 
 #define METER_TASK_PRIORITY             (tskIDLE_PRIORITY + 3)
-#define METER_STACK_SIZE                (128)
+#define METER_STACK_SIZE                (256)
 
 #define FILTER_TASK_PRIORITY             (tskIDLE_PRIORITY + 3)
 #define FILTER_STACK_SIZE                (256)
